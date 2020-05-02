f57bdc8f7d804585bbb522e76d4a1ebcaae30fda
weimingtom2000 <weimingtom@163.com>
2020-05-02 10:47:31 +0800

    mod

---
+++ LuaScriptCore/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cn.vimfung.luascriptcore
+{
+	public class Application
+	{
+		public Application()
+		{
+		}
+		
+		public static String streamingAssetsPath
+		{
+			get
+			{
+				return ".\\";
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using cn.vimfung.luascriptcore;
+using System.Diagnostics;
+
+namespace luascriptcore
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			LuaContext.currentContext.onException((errMessage) => {
+				Debug.WriteLine(errMessage);
+            });			
+			LuaValue retValue = LuaContext.currentContext.evalScript ("print(10);return 'Hello World abc';");
+			Debug.WriteLine (string.Format("{0}", retValue.toString()));
+            Console.WriteLine("return value: " + retValue.toString());
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
